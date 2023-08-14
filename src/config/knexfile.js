c12317e6d1e71393d22c6e66f2b3d03d1eeeba82
MatheusDesenvolvedor <matheuswalace56@gmail.com>
2023-08-14 10:37:17 -0300

    feat: add knexfile

---
+++ src/config/knexfile.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const path = require("path");
+
+module.exports = {
+  client: "mysql",
+  connection: {
+    host: process.env.DBHOST,
+    user: process.env.DBNAME,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBUSER,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+    acquireTimeoutMillis: 10000,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+    directory: path.resolve(__dirname, "./migrations"),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, "./seeds"),
+  },
+};
