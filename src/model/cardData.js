8ff835aa0a41408aded02bf9488ffb005a6f0ea6
MatheusDesenvolvedor <matheuswalace56@gmail.com>
2023-08-14 11:32:04 -0300

    feat: add model

---
+++ src/model/cardData.js
@@ -0,0 +1,13 @@
+const Joi = require("joi");
+
+const object = Joi.object().keys({
+  id: Joi.number(),
+  card_number: Joi.string().length(16).required(),
+  expiration_date: Joi.number().integer().required(),
+  cvv: Joi.number().integer().required(),
+  card_name: Joi.string().required(),
+});
+
+module.exports = {
+  object,
+};
